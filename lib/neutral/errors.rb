87b12f2113443b0753a066398b4e0a069c1c4252
Peter <proximin@gmail.com>
2014-01-22 17:54:31 +0100

    unused constant removal

--- lib/neutral/errors.rb
+++ lib/neutral/errors.rb
@@ -2,7 +2,6 @@ module Neutral
   module Errors
     class InvalidVoteableObject < StandardError;end
     class RequireLogin < StandardError;end
-    class InvalidVoterInstance < StandardError;end
     class CannotChange < StandardError;end
     class DuplicateVote < StandardError;end
     class UndefinedIconSet < StandardError;end
