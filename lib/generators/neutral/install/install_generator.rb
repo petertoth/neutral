e8d1ce3e986f255e5024d85537d86261f8c245ac
Peter Toth <peter@petertoth.me>
2017-02-14 02:58:49 +0100

    change migration numbering

--- lib/generators/neutral/install/install_generator.rb
+++ lib/generators/neutral/install/install_generator.rb
@@ -48,7 +48,7 @@ module Neutral
 
       def self.next_migration_number(dirname)
         if ActiveRecord::Base.timestamped_migrations
-          Time.now.utc.strftime("%Y%m%d%H%M%S%6N")
+          Time.now.utc.strftime("%Y%m%d%H%M%S")
         else
           "%.3d" % (current_migration_number(dirname) + 1)
         end
--- lib/generators/neutral/uninstall/uninstall_generator.rb
+++ lib/generators/neutral/uninstall/uninstall_generator.rb
@@ -44,7 +44,7 @@ module Neutral
       private
       def self.next_migration_number(dirname)
         if ActiveRecord::Base.timestamped_migrations
-          Time.now.utc.strftime("%Y%m%d%H%M%S%6N")
+          Time.now.utc.strftime("%Y%m%d%H%M%S")
         else
           "%.3d" % (current_migration_number(dirname) + 1)
         end
